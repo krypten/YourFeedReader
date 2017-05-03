2245bc17a8e9c7a063b3d4d68a9db781675b7c02
krypten <chaitiagrawal@gmail.com>
2017-05-03 18:35:35 +0530

    fix: Removed use of deprecated APIs and removed unused class
    * The method setOnPageChangeListener is deprecated. Removed the use of this by the method addOnPageChangeListener.
    * The method Palette.generate() is deprecated. Removed the use of this by the method from(Bitmap).generate().
    * Removed the unused class DividerItemDecoration.java from app.
    
    Resolves #13, resolves #14, resolves #15

--- app/src/main/java/com/udacity/feedreader/ui/ArticleDetailActivity.java
+++ app/src/main/java/com/udacity/feedreader/ui/ArticleDetailActivity.java
@@ -56,7 +56,7 @@ public class ArticleDetailActivity extends AppCompatActivity
 				.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics()));
 		mPager.setPageMarginDrawable(new ColorDrawable(0x22000000));
 
-		mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+		mPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
 			@Override
 			public void onPageScrollStateChanged(final int state) {
 				super.onPageScrollStateChanged(state);
--- app/src/main/java/com/udacity/feedreader/ui/ArticleDetailFragment.java
+++ app/src/main/java/com/udacity/feedreader/ui/ArticleDetailFragment.java
@@ -41,7 +41,7 @@ import java.util.GregorianCalendar;
  * tablets) or a {@link ArticleDetailActivity} on handsets.
  */
 public class ArticleDetailFragment extends Fragment implements
-		LoaderManager.LoaderCallbacks<Cursor> {
+	LoaderManager.LoaderCallbacks<Cursor> {
 	private static final String TAG = "ArticleDetailFragment";
 
 	public static final String ARG_ITEM_ID = "item_id";
@@ -93,7 +93,7 @@ public class ArticleDetailFragment extends Fragment implements
 
 		mIsCard = getResources().getBoolean(R.bool.detail_is_card);
 		mStatusBarFullOpacityBottom = getResources().getDimensionPixelSize(
-				R.dimen.detail_card_top_margin);
+			R.dimen.detail_card_top_margin);
 		setHasOptionsMenu(true);
 	}
 
@@ -117,7 +117,7 @@ public class ArticleDetailFragment extends Fragment implements
 	                         Bundle savedInstanceState) {
 		mRootView = inflater.inflate(R.layout.fragment_article_detail, container, false);
 		mDrawInsetsFrameLayout = (DrawInsetsFrameLayout)
-				mRootView.findViewById(R.id.draw_insets_frame_layout);
+			mRootView.findViewById(R.id.draw_insets_frame_layout);
 		mDrawInsetsFrameLayout.setOnInsetsCallback(new DrawInsetsFrameLayout.OnInsetsCallback() {
 			@Override
 			public void onInsetsChanged(Rect insets) {
@@ -145,9 +145,9 @@ public class ArticleDetailFragment extends Fragment implements
 			@Override
 			public void onClick(View view) {
 				startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(getActivity())
-						.setType("text/plain")
-						.setText("Some sample text")
-						.getIntent(), getString(R.string.action_share)));
+					.setType("text/plain")
+					.setText("Some sample text")
+					.getIntent(), getString(R.string.action_share)));
 			}
 		});
 
@@ -160,12 +160,12 @@ public class ArticleDetailFragment extends Fragment implements
 		int color = 0;
 		if (mPhotoView != null && mTopInset != 0 && mScrollY > 0) {
 			float f = progress(mScrollY,
-					mStatusBarFullOpacityBottom - mTopInset * 3,
-					mStatusBarFullOpacityBottom - mTopInset);
+				mStatusBarFullOpacityBottom - mTopInset * 3,
+				mStatusBarFullOpacityBottom - mTopInset);
 			color = Color.argb((int) (255 * f),
-					(int) (Color.red(mMutedColor) * 0.9),
-					(int) (Color.green(mMutedColor) * 0.9),
-					(int) (Color.blue(mMutedColor) * 0.9));
+				(int) (Color.red(mMutedColor) * 0.9),
+				(int) (Color.green(mMutedColor) * 0.9),
+				(int) (Color.blue(mMutedColor) * 0.9));
 		}
 		mStatusBarColorDrawable.setColor(color);
 		mDrawInsetsFrameLayout.setInsetBackground(mStatusBarColorDrawable);
@@ -216,45 +216,52 @@ public class ArticleDetailFragment extends Fragment implements
 			final Date publishedDate = parsePublishedDate();
 			if (!publishedDate.before(START_OF_EPOCH.getTime())) {
 				bylineView.setText(Html.fromHtml(
-						DateUtils.getRelativeTimeSpanString(
-								publishedDate.getTime(),
-								System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
-								DateUtils.FORMAT_ABBREV_ALL).toString()
-								+ " by <font color='#ffffff'>"
-								+ mCursor.getString(ArticleLoader.Query.AUTHOR)
-								+ "</font>"));
+					DateUtils.getRelativeTimeSpanString(
+						publishedDate.getTime(),
+						System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
+						DateUtils.FORMAT_ABBREV_ALL).toString()
+						+ " by <font color='#ffffff'>"
+						+ mCursor.getString(ArticleLoader.Query.AUTHOR)
+						+ "</font>"));
 
 			} else {
 				// If date is before 1902, just show the string
 				bylineView.setText(Html.fromHtml(
-						outputFormat.format(publishedDate) + " by <font color='#ffffff'>"
-								+ mCursor.getString(ArticleLoader.Query.AUTHOR)
-								+ "</font>"));
+					outputFormat.format(publishedDate) + " by <font color='#ffffff'>"
+						+ mCursor.getString(ArticleLoader.Query.AUTHOR)
+						+ "</font>"));
 
 			}
 			bodyView.setText(Html.fromHtml(mCursor.getString(ArticleLoader.Query.BODY).replaceAll("(\r\n|\n)", "<br />")));
 			ImageLoaderHelper.getInstance(getActivity()).getImageLoader()
-					.get(mCursor.getString(ArticleLoader.Query.PHOTO_URL), new ImageLoader.ImageListener() {
-						@Override
-						public void onResponse(ImageLoader.ImageContainer imageContainer, boolean b) {
-							final Bitmap bitmap = imageContainer.getBitmap();
-							if (bitmap != null) {
-								final Palette p = Palette.generate(bitmap, 12);
-								mMutedColor = p.getDarkVibrantColor(mMutedColor);
-								mPhotoView.setImageBitmap(bitmap);
-								final View shareFab = mRootView.findViewById(R.id.share_fab);
-								if (shareFab != null) {
-									((LayerDrawable) shareFab.getBackground()).getDrawable(0).setColorFilter(mMutedColor, PorterDuff.Mode.MULTIPLY);
-								}
-								updateStatusBar();
-							}
+				.get(mCursor.getString(ArticleLoader.Query.PHOTO_URL), new ImageLoader.ImageListener() {
+					@Override
+					public void onResponse(ImageLoader.ImageContainer imageContainer, boolean b) {
+						final Bitmap bitmap = imageContainer.getBitmap();
+						if (bitmap != null) {
+							Palette.from(bitmap)
+								.maximumColorCount(12)
+								.generate(new Palette.PaletteAsyncListener() {
+									public void onGenerated(final Palette p) {
+										mMutedColor = p.getDarkVibrantColor(mMutedColor);
+										mPhotoView.setImageBitmap(bitmap);
+										final View shareFab = mRootView.findViewById(R.id.share_fab);
+										if (shareFab != null) {
+											((LayerDrawable) shareFab.getBackground())
+												.getDrawable(0)
+												.setColorFilter(mMutedColor, PorterDuff.Mode.MULTIPLY);
+										}
+									}
+								});
+							updateStatusBar();
 						}
+					}
 
-						@Override
-						public void onErrorResponse(VolleyError volleyError) {
+					@Override
+					public void onErrorResponse(VolleyError volleyError) {
 
-						}
-					});
+					}
+				});
 		} else {
 			mRootView.setVisibility(View.GONE);
 			titleView.setText("N/A");
@@ -283,7 +290,6 @@ public class ArticleDetailFragment extends Fragment implements
 			mCursor.close();
 			mCursor = null;
 		}
-
 		bindViews();
 	}
 
@@ -297,10 +303,9 @@ public class ArticleDetailFragment extends Fragment implements
 		if (mPhotoContainerView == null || mPhotoView.getHeight() == 0) {
 			return Integer.MAX_VALUE;
 		}
-
 		// account for parallax
 		return mIsCard
-				? (int) mPhotoContainerView.getTranslationY() + mPhotoView.getHeight() - mScrollY
-				: mPhotoView.getHeight() - mScrollY;
+			? (int) mPhotoContainerView.getTranslationY() + mPhotoView.getHeight() - mScrollY
+			: mPhotoView.getHeight() - mScrollY;
 	}
 }
